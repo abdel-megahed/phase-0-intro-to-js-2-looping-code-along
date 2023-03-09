bee8e09b1b833f7fe5a22dfa4a3d765c07118e90
abdel <ammegahe@amazon.com>
2023-03-09 18:21:25 -0500

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, occasion) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${occasion} gift!`);
+    }
+  
+    return thankYouMessages;
+  }
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
